8f56580008e71a976998d8c66611ec0708afe149
Puru Dahal <puru@dahal.me>
2019-09-12 21:54:03 -0700

    Adds extra level of security to ensure an invite is never sent to a user that shouldn't receive one.

--- app/controllers/administrator_invitations_controller.rb
+++ app/controllers/administrator_invitations_controller.rb
@@ -5,8 +5,13 @@ class AdministratorInvitationsController < ApplicationController
   # PUT /admin_invitations/:id
   # PATCH /admin_invitations/:id
   def update
-    @user.invite!
-    flash[:success] = t('devise.invitations.send_instructions', email: @user.email)
+    if !@user.accepted_or_not_invited?
+      @user.invite!
+      flash[:success] = t('devise.invitations.send_instructions', email: @user.email)
+    else
+      flash[:success] = t('devise.invitations.not_sent', email: @user.email)
+    end
+
     redirect_to user_path(@user)
   end
 
